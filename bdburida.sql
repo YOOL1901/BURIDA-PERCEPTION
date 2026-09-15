199fe4fd01e836da23650d4fcba93bc9778080ef
YOOL1901 <74843759+YOOL1901@users.noreply.github.com>
2026-09-15 09:39:56 +0000

    Modify database connection in bdburida.sql
    Updated database connection parameters to empty values.

--- bdburida.sql
+++ bdburida.sql
@@ -3,7 +3,7 @@ session_start();
 $_SESSION['moment'] = time();
 try
 {
-    $bdd = new PDO('mysql:host=185.98.131.214;dbname=tabit2577523_1h14da;charset=utf8', 'tabit2577523', 'nopcxdesqb');
+    $bdd = new PDO('mysql:host=;dbname=;charset=utf8', '', '');
 }
 catch (Exception $e)
 {
